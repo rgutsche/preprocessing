af3cb3e9cb28e95b1c5f37de9ca60a0f04b6cbe4
rgutsche <r.gutsche@fz-juelich.de>
2023-02-25 10:56:15 +0100

    First Commit

--- preprocessing/run_v2.py
+++ preprocessing/run_v2.py
@@ -185,18 +185,18 @@ def run_preprocessing(pid, queue, configurer):
 #     # #     out_file = settings.intermediate_path.joinpath(settings.project, pid, f'{sequence}_reorient.nii.gz')
 #     # #     reorient_2_std(in_file, out_file)
 #
-#     #%% 2) Registration
-#     sequences = ['t1_km', 't2', 'flair', 'pet']
-#
-#     for sequence in sequences:
-#         print(f'PID: {pid}. Registration for: {sequence}')
-#
-#         in_file = settings.intermediate_path.joinpath(settings.project, pid, f'{sequence}.nii.gz')
-#         ref_file = settings.intermediate_path.joinpath(settings.project, pid, f't1_native.nii.gz')
-#         out_file = settings.intermediate_path.joinpath(settings.project, pid, f'{sequence}_co.nii.gz')
-#         registration(in_file, ref_file, out_file)
-#
-#     print(f'PID: {pid}. Registration for: {sequence} done')
+    #%% 2) Registration
+    sequences = ['t1_km', 't2', 'flair', 'pet']
+
+    for sequence in sequences:
+        print(f'PID: {pid}. Registration for: {sequence}')
+
+        in_file = settings.intermediate_path.joinpath(settings.project, pid, f'{sequence}.nii.gz')
+        ref_file = settings.intermediate_path.joinpath(settings.project, pid, f't1_native.nii.gz')
+        out_file = settings.intermediate_path.joinpath(settings.project, pid, f'{sequence}_co.nii.gz')
+        registration(in_file, ref_file, out_file)
+
+    print(f'PID: {pid}. Registration for: {sequence} done')
 #
     #%% 3) Brain Segmentation
     print(f'PID: {pid}. Brain segmentation')
